052bdfca987e6120ce4135ae40cf327e4d442cf3
Femi Mehalayese <megafemclick@gmail.com>
2024-10-28 13:52:30 +0100

    p

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -40,11 +40,12 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """Get numbers of pages"""
+        """Retrieves a page of data.
+        """
         assert type(page) == int and type(page_size) == int
         assert page > 0 and page_size > 0
-        i, j = index_range(page, page_size)
-        data_set = self.dataset()
-        if i > len(data_set):
+        start, end = index_range(page, page_size)
+        data = self.dataset()
+        if start > len(data):
             return []
-        return data_set[i:j]
+        return data[start:end]
