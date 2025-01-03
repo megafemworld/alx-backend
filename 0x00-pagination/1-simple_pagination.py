3416b40feb3ddab277aa5dcd3ff11d40a51cf262
Femi Mehalayese <megafemclick@gmail.com>
2025-01-03 20:18:44 +0100

    doc for 1 8

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,14 +1,7 @@
 #!/usr/bin/env python3
-from typing import Tuple
+from typing import Tuple, List
 import csv
 import math
-from typing import List
-"""
-    The function should return a tuple of size two
-    containing a start index and an end index corresponding
-    to the range of indexes to return in a list
-    for those particular pagination parameters.
-"""
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, ...]:
